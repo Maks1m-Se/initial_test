bdccf228e099ec6de99fe4f719dacf482ee92022
Maksim Sendetski <maksim.sendetski@29forward.de>
2023-10-12 16:29:10 +0200

    functionality of select_all_nations button

--- components/island_dropdown.py
+++ components/island_dropdown.py
@@ -1,8 +1,18 @@
 from dash import Dash, html, dcc
+from dash.dependencies import Input, Output
 from . import ids
 
 def render(app: Dash) -> html.Div:
     all_islands = ['Torgersen', 'Biscoe', 'Dream']
+
+    # implementing functionality to add all islands to dropdown
+    @app.callback(
+        Output(ids.ISLAND_DROPDOWN, "value"),
+        Input(ids.SELECT_ALL_ISLANDS_BUTTON, "n_clicks")
+    )
+    def select_all_islands(_: int) -> list[str]:
+        return all_islands
+    
     return html.Div(
         children=[
             html.H6('Island'),
